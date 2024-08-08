875087ef3689564780c815f341457e85de95c22b
Sei Munkhbat <sei@Seis-MacBook-Pro.local>
2024-08-08 14:57:16 -0500

    Added github links to portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,46 +1,39 @@
 import React from 'react';
+import '../assets/css/app.css';
+import githubIcon from '../assets/images/github-icon.png'; 
 
 const Portfolio = () => (
   <section id="my-works" className="my-works">
     <div className="work">
-      <a href="https://sei-cloud.github.io/CSS-Snippet/">
-        <header>CSS-Snippet</header>
+      <a href="https://sei-cloud.github.io/CSS-Snippet/" target="_blank" rel="noopener noreferrer">
+        <header className="work-header">
+          CSS-Snippet
+          <a href="https://github.com/Sei-cloud/css-snippet" target="_blank" rel="noopener noreferrer" className="github-link">
+            <img src={githubIcon} alt="GitHub" className="github-icon" />
+          </a>
+        </header>
         <img src="./assets/images/CSS-Snippet.png" alt="CSS Snippet" />
       </a>
     </div>
     <div className="work">
-      <a href="https://sei-cloud.github.io/Horiseon-Home/">
-        <header>Horiseon-WebPage</header>
-        <img
-          src="./assets/images/digital-marketing-meeting.jpg"
-          alt="Horiseon"
-        />
+      <a href="https://sei-cloud.github.io/Horiseon-Home/" target="_blank" rel="noopener noreferrer">
+        <header className="work-header">
+          Horiseon-WebPage
+          <a href="https://github.com/Sei-cloud/horiseon-home" target="_blank" rel="noopener noreferrer" className="github-link">
+            <img src={githubIcon} alt="GitHub" className="github-icon" />
+          </a>
+        </header>
+        <img src="./assets/images/digital-marketing-meeting.jpg" alt="Horiseon" />
       </a>
     </div>
     <div className="work">
-      <a href="https://sei-cloud.github.io/mini-project/">
-        <header>Demo WebPage</header>
-        <img src="./assets/images/Demo-Webpage.png" alt="Demo Webpage" />
-      </a>
-    </div>
-    <div className="work">
-      <a href="https://sei-cloud.github.io/CSS-Snippet/">
-        <header>CSS-Snippet</header>
-        <img src="./assets/images/CSS-Snippet.png" alt="CSS Snippet" />
-      </a>
-    </div>
-    <div className="work">
-      <a href="https://sei-cloud.github.io/Horiseon-Home/">
-        <header>Horiseon-WebPage</header>
-        <img
-          src="./assets/images/digital-marketing-meeting.jpg"
-          alt="Horiseon"
-        />
-      </a>
-    </div>
-    <div className="work">
-      <a href="https://sei-cloud.github.io/mini-project/">
-        <header>Demo WebPage</header>
+      <a href="https://sei-cloud.github.io/mini-project/" target="_blank" rel="noopener noreferrer">
+        <header className="work-header">
+          Demo WebPage
+          <a href="https://github.com/Sei-cloud/mini-project" target="_blank" rel="noopener noreferrer" className="github-link">
+            <img src={githubIcon} alt="GitHub" className="github-icon" />
+          </a>
+        </header>
         <img src="./assets/images/Demo-Webpage.png" alt="Demo Webpage" />
       </a>
     </div>
